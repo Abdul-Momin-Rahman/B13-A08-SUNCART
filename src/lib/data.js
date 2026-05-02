c78430b219fa9d70104c1be548fe606a32c95eb8
Abdul-Momin-Rahman <a.mominatick@gmail.com>
2026-05-02 19:22:56 +0600

    json server is added

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,13 +1,23 @@
-import products from "@/data/products.json"
+
 
 export const getProductsData =  async() => {
-    const res = await fetch("http://localhost:5000/products");
+    const res = await fetch("https://suncart-server-t99d.onrender.com/products");
+
+    if (!res.ok) {
+        throw new Error ("Failed to fetch data");
+    }
+
     const data = await res.json();
     return data;
 }
 
 export const getProductsDataById = async(id) => {
-    const res = await fetch(`http://localhost:5000/products/${id}`);
+    const res = await fetch(`https://suncart-server-t99d.onrender.com/products/${id}`);
+
+    if (!res.ok) {
+        throw new Error ("Failed to fetch data");
+    }
+
     const data = await res.json();
     return data
 }
